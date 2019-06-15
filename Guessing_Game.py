00742e685594c7a94544b6dbe773a6b609e10a3a
badladpancho <51802107+badladpancho@users.noreply.github.com>
2019-06-14 20:48:57 -0700

    Add files via upload

---
+++ Guessing_Game.py
@@ -0,0 +1,20 @@
+# This is going to be a simple game in order to guess
+# We are going to be using if statments while loops and other things that i have
+# learned.
+print("Player 1 enter the secret word\n");
+secret_word = input("");
+print("OK lets play!");
+guess = ""
+i = 0;
+chance_limit = 3;
+while guess != secret_word:
+    guess = input("Player 2 Enter the guess word:\n");
+    if guess == secret_word:
+        print("You guessed the right word!\n");
+        break;
+    if i >= 3:
+        print("You ran out of chances to guess the word\n");
+        break;
+    i += 1;
+    chance_limit -= 1;
+    print("you have " + str(chance_limit)+ " chances left");
---
+++ While_loop.py
@@ -0,0 +1,8 @@
+#   This is the basic to a while loop
+#   will loop through until it is false
+#   This is like it is done in C programing
+i = 0
+while i <= 10:
+    print(i);
+    i += 1;
+print("Done with this loop")
