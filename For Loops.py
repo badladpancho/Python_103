e29fae8fa5feeb81c17468619e4bf3fbab420d02
badladpancho <51802107+badladpancho@users.noreply.github.com>
2019-06-26 18:44:09 -0700

    Add files via upload

---
+++ For Loops.py
@@ -0,0 +1,8 @@
+#Loops are useful for many things.
+#looping through a string, arry, and other data stuctures
+friends = ["Javier", "steph", "bob"]
+for index in range(5): # range starts count from 0
+    if index == 0:
+        print("This is the first loop")
+    else:
+        print("Not first")
